4b257d6f70da9a878ca6c5b360987ef1dbcf1a47
zraim <zraim.ebrada1@gmail.com>
2026-03-04 19:56:27 +0800

    Finished Part1 commit (added working buttons)

--- app/src/main/java/com/example/part1exercise01/MainActivity.java
+++ app/src/main/java/com/example/part1exercise01/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.part1exercise01;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +15,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("ID: 22102083");
+        }
+
+        Button myButton = findViewById(R.id.btnChangeName);
+        final TextView myText = findViewById(R.id.dcismdept);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myText.setText("Ms. Angie M. Ceniza-Canillo, PhD");
+            }
         });
     }
 }
